68349678cc4151f6cc473369bd9f728441cb6d18
Prahalad Belavadi <github@belavadi.com>
2020-05-24 03:51:34 +0530

    state: Never modify state directly

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import React, { Component } from "react";
 class App extends Component {
   constructor() {
     super();
-    this.state = { displayBio: true };
+    this.state = { displayBio: false };
+
+    // this.readMore = this.readMore.bind(this);
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+  }
+
+  // readMore() {
+  //   // bad practice
+  //   // this.state.displayBio = true;
+
+  //   // Use this instead
+  //   this.setState({ displayBio: true });
+  // }
+
+  // showLess() {
+  //   this.setState({ displayBio: false });
+  // }
+
+  toggleDisplayBio() {
+    this.setState({ displayBio: !this.state.displayBio });
   }
 
   render() {
-    // const bio = this.state.displayBio ? (
-    //   <div>
-    //     <p>I live in Bengaluru, India and I write code almost everyday.</p>
-    //     <p>
-    //       I speak Kannada, Hindi and English; and parts of broken Spanish and
-    //       French.
-    //     </p>
-    //     <p> I Love frozen yogurt. </p>
-    //   </div>
-    // ) : null;
-
-    // if (!this.state.displayBio) {
-    //   bio = null;
-    // }
-
     return (
       <div>
         <h1>Hello</h1>
@@ -36,8 +40,14 @@ class App extends Component {
               and French.
             </p>
             <p> I Love frozen yogurt. </p>
+            <button onClick={this.toggleDisplayBio}> Show Less </button>
           </div>
-        ) : null}
+        ) : (
+          <div>
+            {/* <button onClick={this.readMore}> Read more</button> */}
+            <button onClick={this.toggleDisplayBio}> Read more</button>
+          </div>
+        )}
       </div>
     );
   }
