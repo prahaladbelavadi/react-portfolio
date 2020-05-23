9db416cac5bc1026f17f7ff9dc911f4a69740f75
Prahalad Belavadi <github@belavadi.com>
2020-05-24 03:08:35 +0530

    use ternary operators to render html based on if else programs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from "react";
 
-// Understand Differences between Regular class and extended class
-class RegularClass {}
-class ComponentClass extends Component {}
-
-const RegularClassInstance = new RegularClass();
-const ComponentClassInstance = new ComponentClass();
-
-console.log("RegularClassInstance", RegularClassInstance);
-console.log("ComponentClassInstance", ComponentClassInstance);
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: true };
+  }
+
   render() {
+    // const bio = this.state.displayBio ? (
+    //   <div>
+    //     <p>I live in Bengaluru, India and I write code almost everyday.</p>
+    //     <p>
+    //       I speak Kannada, Hindi and English; and parts of broken Spanish and
+    //       French.
+    //     </p>
+    //     <p> I Love frozen yogurt. </p>
+    //   </div>
+    // ) : null;
+
+    // if (!this.state.displayBio) {
+    //   bio = null;
+    // }
+
     return (
       <div>
         <h1>Hello</h1>
         <p>My name is Prahalad and I'm a Software developer.</p>
         <p>I'm easily excited!</p>
+        {/* {bio} */}
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Bengaluru, India and I write code almost everyday.</p>
+            <p>
+              I speak Kannada, Hindi and English; and parts of broken Spanish
+              and French.
+            </p>
+            <p> I Love frozen yogurt. </p>
+          </div>
+        ) : null}
       </div>
     );
   }
