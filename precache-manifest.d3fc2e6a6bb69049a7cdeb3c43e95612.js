3159bd38cf595a238c6be0f3f9189f5ef9463ad4
Prahalad Belavadi <github@belavadi.com>
2020-05-25 01:47:20 +0530

    Updates

---
+++ precache-manifest.d3fc2e6a6bb69049a7cdeb3c43e95612.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ba563fd930438126238bef70982aeb",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0e6553f50ab829d803f5",
+    "url": "/react-portfolio/static/css/main.2b4c6f9a.chunk.css"
+  },
+  {
+    "revision": "90c8ce501c4c0c522acd",
+    "url": "/react-portfolio/static/js/2.cef0fc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.cef0fc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6553f50ab829d803f5",
+    "url": "/react-portfolio/static/js/main.5ecb194c.chunk.js"
+  },
+  {
+    "revision": "be3cb1a3dfa81366eb8c",
+    "url": "/react-portfolio/static/js/runtime-main.5ca49836.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/react-portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/react-portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/react-portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/react-portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "9e286e8bf57e945dbbaadff88dcd1ba5",
+    "url": "/react-portfolio/static/media/profile.9e286e8b.png"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/react-portfolio/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/react-portfolio/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/react-portfolio/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/react-portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/react-portfolio/static/media/twitter_icon.512536cb.png"
+  }
+]);
